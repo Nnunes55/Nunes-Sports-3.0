00375d6d31e03475f66cf54c31e900c7dfc6c164
Nathan Nunes <nathannunessp587@gmail.com>
2024-01-26 13:32:18 -0300

    Atualizações

--- pages/signin/script.js
+++ pages/signin/script.js
@@ -2,12 +2,12 @@ function showSenha() {
   var inputpass = document.getElementById("senha");
   var btnShowPass = document.getElementById("btn-senha");
 
-  if (inputpass.type === 'password') {
-    inputpass.setAttribute('type','text');
-    btnShowPass.classList.replace('bi-eye-fill','bi-eye-slash-fill');
-  }else{
-    inputpass.setAttribute('type','password');
-    btnShowPass.classList.replace('bi-eye-slash-fill','bi-eye-fill');
+  if (inputpass.type === "password") {
+    inputpass.setAttribute("type", "text");
+    btnShowPass.classList.replace("bi-eye-fill", "bi-eye-slash-fill");
+  } else {
+    inputpass.setAttribute("type", "password");
+    btnShowPass.classList.replace("bi-eye-slash-fill", "bi-eye-fill");
   }
 }
 
@@ -15,37 +15,33 @@ function showSenha1() {
   var inputpass = document.getElementById("confirmeSenha");
   var btnShowPass = document.getElementById("btn-senha1");
 
-  if (inputpass.type === 'password') {
-    inputpass.setAttribute('type','text');
-    btnShowPass.classList.replace('bi-eye-fill','bi-eye-slash-fill');
-  }else{
-    inputpass.setAttribute('type','password');
-    btnShowPass.classList.replace('bi-eye-slash-fill','bi-eye-fill');
+  if (inputpass.type === "password") {
+    inputpass.setAttribute("type", "text");
+    btnShowPass.classList.replace("bi-eye-fill", "bi-eye-slash-fill");
+  } else {
+    inputpass.setAttribute("type", "password");
+    btnShowPass.classList.replace("bi-eye-slash-fill", "bi-eye-fill");
   }
 }
 
-
-
-
-
-const form = document.querySelector("#form")
-form.addEventListener("submit" , (e)=>{
-  e.preventDefault()
-  const email = document.querySelector("#email").value
-  const senha = document.querySelector("#senha").value
-  const confirmeSenha = document.querySelector("#confirmeSenha").value
+const form = document.querySelector("#form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = document.querySelector("#email").value;
+  const senha = document.querySelector("#senha").value;
+  const confirmeSenha = document.querySelector("#confirmeSenha").value;
 
   if (!email) {
     alert("Por favor, preencha o email.");
-    return; 
-  }else if (!email.includes(".com")) { 
+    return;
+  } else if (!email.includes(".com")) {
     alert("Por favor, informe um email válido com a terminação '.com'.");
     return;
   }
   if (!senha) {
     alert("Por favor, preencha a senha.");
     return;
-  }else if (senha.length < 4) {
+  } else if (senha.length < 4) {
     alert("A senha deve ter pelo menos 4 caracteres.");
     return;
   }
@@ -53,15 +49,15 @@ form.addEventListener("submit" , (e)=>{
     alert("As senhas não conferem.");
     return;
   }
-  
-  const users = JSON.parse(localStorage.getItem('users')) || []
-  const isUserRegistered = users.find(user => user.email === email)
-  if(isUserRegistered){
-    return alert("O usuario ja está Cadastrado!")
+
+  const users = JSON.parse(localStorage.getItem("users")) || [];
+  const isUserRegistered = users.find((user) => user.email === email);
+  if (isUserRegistered) {
+    return alert("O usuario ja está Cadastrado!");
   }
 
-  users.push({ email, senha, confirmeSenha, role: "user"})
-  localStorage.setItem('users', JSON.stringify(users))
-  alert('Usuario cadastrado com sucesso!')
-  window.location.href = '/'
-})
+  users.push({ email, senha, confirmeSenha, role: "user" });
+  localStorage.setItem("users", JSON.stringify(users));
+  alert("Usuario cadastrado com sucesso!");
+  window.location.href = "/";
+});
